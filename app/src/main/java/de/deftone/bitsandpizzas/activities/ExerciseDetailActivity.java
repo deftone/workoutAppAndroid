c107f98a06de6f57897d2788259adf1ab0438542
Katrin <webentwicklerinKatrin@gmail.com>
2018-01-08 22:01:34 +0100

    buttons inkl shared prefs zum punktesammeln hinzugefuegt

--- app/src/main/java/de/deftone/bitsandpizzas/activities/ExerciseDetailActivity.java
+++ app/src/main/java/de/deftone/bitsandpizzas/activities/ExerciseDetailActivity.java
@@ -1,6 +1,8 @@
 package de.deftone.bitsandpizzas.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
@@ -8,17 +10,22 @@ import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import de.deftone.bitsandpizzas.R;
 import de.deftone.bitsandpizzas.data.BackExercise;
 import de.deftone.bitsandpizzas.data.BellyExercise;
+import de.deftone.bitsandpizzas.data.Icons;
 import de.deftone.bitsandpizzas.data.LegExercise;
 import de.deftone.bitsandpizzas.data.StretchingExercise;
 import de.deftone.bitsandpizzas.utils.ExerciseDetailList;
@@ -34,29 +41,46 @@ public class ExerciseDetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_EXERCISE_ID = "exercise_id";
     public static final String EXTRA_EXERCISE_TYPE = "exercise_type";
+    public static final String PREFS_NAME = "exercises_date_points";
     ListView list;
+    String title = "";
+    int image = 0;
+    String[] icon = {};
+    String[] desc = {};
+    int seconds = 0;
+    int[] weight = {};
+    Button weightButton;
+    Button weightButton2;
+    Button weightButton3;
+    Button weightAlternButton;
+    Button weightAlternButton2;
+    Button weightAlternButton3;
+    SharedPreferences sharedPreferences;
+    boolean buttonClicked = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises_detail);
 
-        //scroll to top
-        NestedScrollView scrollView = findViewById(R.id.detail_scrollview);
-        scrollView.getParent().requestChildFocus(scrollView, scrollView);
-
-
-        //Display details of the exercise
+        //get details of the exercise
         int id = getIntent().getExtras().getInt(EXTRA_EXERCISE_ID);
         String type = getIntent().getExtras().getString(EXTRA_EXERCISE_TYPE);
+        getDescriptionDetails(id, type);
 
-        String title = "";
-        int image = 0;
-        String[] icon = {};
-        String[] desc = {};
-        int seconds = 0;
-        int[] weight = {};
+        //scroll to top and adjust/set elements on activity layout
+        NestedScrollView scrollView = findViewById(R.id.detail_scrollview);
+        scrollView.getParent().requestChildFocus(scrollView, scrollView);
+        setToobar();
+        setImageAndDescription();
+        setWeightButtons(weight);
+        buttonClicked = false;
+        setFab();
+    }
 
+
+    private void getDescriptionDetails(int id, String type) {
         switch (type) {
             case LEG_EXERCISES:
                 title = LegExercise.LEG_EXERCISES[id].getName();
@@ -99,44 +123,124 @@ public class ExerciseDetailActivity extends AppCompatActivity {
                 weight = CREATED_EXERCISES_LIST.get(id).getWeight();
                 break;
         }
+    }
 
+    private void setToobar() {
         Toolbar toolbar = findViewById(R.id.toolbar_detail);
         toolbar.setTitle(title);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         //todo: das hier soll sich so verhalten wie der back button von android, erst dann auf true setzen
         actionBar.setDisplayHomeAsUpEnabled(false);
-
+//in case i will ever need this:
 //        Now If you want to use CollapsingToolbarLayout and Toolbar together then you have to use setTitle() of CollapsingToolbarLayout
 //        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.confirm_order_mail_layout);
 //        collapsingToolbarLayout.setTitle("My Title");
+    }
 
-
+    private void setImageAndDescription() {
         ImageView imageView = findViewById(R.id.image_detail);
         imageView.setImageDrawable(ContextCompat.getDrawable(this, image));
 
         ExerciseDetailList adapter = new ExerciseDetailList(ExerciseDetailActivity.this, icon, desc);
         list = findViewById(R.id.list_detail);
         list.setAdapter(adapter);
+    }
 
+    private void setWeightButtons(int[] weight) {
         TableRow secondRow = findViewById(R.id.second_row);
-        TextView text_points = findViewById(R.id.text_points);
-        Button weightButton = findViewById(R.id.weight_button);
+        TextView textView = findViewById(R.id.text_points);
+
+        weightButton = findViewById(R.id.weight_button);
+        weightButton2 = findViewById(R.id.weight_button_2);
+        weightButton3 = findViewById(R.id.weight_button_3);
+        weightButton.setText("1x" + String.valueOf(weight[0]) + Icons.getIcon(Icons.MUSCLE));
+        weightButton2.setText("2x" + String.valueOf(weight[0]) + Icons.getIcon(Icons.MUSCLE));
+        weightButton3.setText("3x" + String.valueOf(weight[0]) + Icons.getIcon(Icons.MUSCLE));
         if (weight.length < 2) {
             secondRow.setVisibility(View.GONE);
-            text_points.setText(R.string.points);
-            weightButton.setText(String.valueOf(weight[0]) + "\u263a");
+            textView.setText(R.string.points);
         } else {
-            //todo: hier stimmt noch etwas nicht...
+            textView.setText(R.string.points_alternative);
             secondRow.setVisibility(View.VISIBLE);
-            text_points.setText(R.string.points_alternative);
-            weightButton.setVisibility(View.GONE);
-            Button button_1 = findViewById(R.id.weight_button_1);
-            button_1.setText(String.valueOf(weight[0]));
-            Button button_2 = findViewById(R.id.weight_button_2);
-            button_2.setText(String.valueOf(weight[1]));
+            weightAlternButton = findViewById(R.id.weight_button_alternative);
+            weightAlternButton2 = findViewById(R.id.weight_button_alternative_2);
+            weightAlternButton3 = findViewById(R.id.weight_button_alternative_3);
+            weightAlternButton.setText("1x" + String.valueOf(weight[1]) + Icons.getIcon(Icons.SPARKLES));
+            weightAlternButton2.setText("2x" + String.valueOf(weight[1]) + Icons.getIcon(Icons.SPARKLES));
+            weightAlternButton3.setText("3x" + String.valueOf(weight[1]) + Icons.getIcon(Icons.SPARKLES));
         }
+    }
 
+    public void onClickPoints(View view) {
+        addPoints(weightButton, weight[0]);
+    }
+
+    public void onClickPoints2(View view) {
+        addPoints(weightButton2, 2 * weight[0]);
+    }
+
+    public void onClickPoints3(View view) {
+        addPoints(weightButton3, 3 * weight[0]);
+    }
+
+    public void onClickPointsAltern(View view) {
+        addPoints(weightAlternButton, weight[1]);
+    }
+
+    public void onClickPointsAltern2(View view) {
+        addPoints(weightAlternButton2, 2 * weight[1]);
+    }
+
+    public void onClickPointsAltern3(View view) {
+        addPoints(weightAlternButton3, 3 * weight[1]);
+    }
+
+    private void addPoints(Button button, int newPoints) {
+        if (!buttonClicked) {
+            //leider muss man das style doppeln und als Drawable benuzen...
+            button.setBackground(getResources().getDrawable(R.drawable.style_shape_rounded_corners_orange));
+            // Restore preferences
+            sharedPreferences = getSharedPreferences(PREFS_NAME, 0);
+            int sumPoints = sharedPreferences.getInt("heute", 0);
+            int newSumPoints = sumPoints + newPoints;
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt("heute", sumPoints + newSumPoints);
+            editor.apply();
+
+            //show toast will sum todo: auslagern! ToastText muss parameter werden
+            //custom toast
+            Context context = getApplicationContext();
+            // Create layout inflator object to inflate toast.xml file
+            LayoutInflater inflater = getLayoutInflater();
+
+            // Call toast.xml file for toast layout
+            View toastview = inflater.inflate(R.layout.toast, null);
+
+            Toast toast = new Toast(context);
+
+            // Set layout to toast
+            toast.setView(toastview);
+            //das hier funktioniert im onclick nicht :( todo: text anpassen
+//            String toastText = "Du hast bisher " + newSumPoints + " Punkte erreicht!";
+//            toast.setText(toastText);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL,
+                    0, 0);
+            toast.setDuration(Toast.LENGTH_SHORT);
+            toast.show();
+            buttonClicked = true;
+        }
+    }
+
+    //for debuggin only
+    private void printPoints() {
+        sharedPreferences = getSharedPreferences(PREFS_NAME, 0);
+        int defaultValue = 0;
+        int sumPoints = sharedPreferences.getInt("heute", defaultValue);
+        Log.d("test", "printPoints: sumPoints = " + sumPoints);
+    }
+
+    public void setFab() {
         FloatingActionButton fab = findViewById(R.id.fab);
         if (seconds > 0) {
             fab.setVisibility(View.VISIBLE);
@@ -154,12 +258,5 @@ public class ExerciseDetailActivity extends AppCompatActivity {
         Intent intentStopwatch = new Intent(this, StopwatchActivity.class);
         intentStopwatch.putExtra(StopwatchActivity.INTENT_SECONDS, sec);
         startActivity(intentStopwatch);
-    }
-
-    public void onClickPoints(View view) {
-        //add sharedPreferences
-        Button weightButton = findViewById(R.id.weight_button);
-        //leider muss man das style doppeln und als Drawable benuzen...
-        weightButton.setBackground(getResources().getDrawable(R.drawable.style_shape_rounded_corners_orange));
     }
 }
